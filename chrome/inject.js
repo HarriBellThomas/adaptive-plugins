cbc8ed70a5b932c5d85882cb766bab28f8c0a9ee
Tristram Newman <tristram.newman@gmail.com>
2018-02-14 17:15:00 +0000

    Initial commit

---
+++ chrome/inject.js
@@ -0,0 +1,1 @@
+document.body.appendChild(function(){(k=document.createElement('script')).src='https://js.adaptive.org.uk/fresh/?negateglobal=N&mod=onDOMChange,adaptiveBase,adaptiveTools,linkHighlighter,init';return k;}());
