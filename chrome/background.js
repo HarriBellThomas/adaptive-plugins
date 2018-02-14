979a0f735fdfc7c26cdae65d7640db826591feed
Tristram Newman <tristram.newman@gmail.com>
2018-02-14 17:39:13 +0000

    Made the browser action open the styles page and added support for Edge 20+

--- chrome/background.js
+++ chrome/background.js
@@ -1,3 +1,6 @@
-chrome.browserAction.onClicked.addListener(function() {
-   chrome.tabs.create({"url": "https://adaptive.org.uk/home"});
+var browserObject = !(!!document.documentMode) && !!window.StyleMedia ?    // If we have Edge 20+
+                    browser : chrome; 
+
+browserObject.browserAction.onClicked.addListener(function() {
+   browserObject.tabs.create({"url": "https://adaptive.org.uk/home"});
 });
