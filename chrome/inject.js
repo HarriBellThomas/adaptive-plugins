05aeef5bed4ddcf343a462586cc05ebe39aea503
Tristram Newman <tristram.newman@gmail.com>
2018-02-20 12:39:57 +0000

    Added some cookie support

--- chrome/inject.js
+++ chrome/inject.js
@@ -1,4 +1,25 @@
-document.body.appendChild(function() {
-   (k = document.createElement('script')).src = 'https://js.adaptive.org.uk/fresh/?negateglobal=N&mod=onDOMChange,adaptiveBase,adaptiveTools,linkHighlighter,init';
-   return k;
-}());
+window.onload = function() {
+   // Inject the script
+   document.body.appendChild(function() {
+      (k = document.createElement('script')).src = 'https://js.adaptive.org.uk/fresh/?negateglobal=N&mod=onDOMChange,adaptiveBase,adaptiveTools,linkHighlighter,init';
+      return k;
+   }());
+   
+   if (window.location.hash) {
+      try {
+         var hash = window.atob(window.location.hash.substr(1, window.location.hash.length - 1));
+         var data = JSON.parse(hash);
+         console.log(data);
+
+         if ("user_id" in data) {
+            chrome.storage.local.set({"ADAPTIVE_A": data["user_id"]});
+            userID = data["user_id"];
+         }
+
+         if ("style_id" in data) {
+            chrome.storage.local.set({"ADAPTIVE_B": data["style_id"]});
+            styleID = data["style_id"];
+         }
+      } catch(e) {}
+   }
+};
