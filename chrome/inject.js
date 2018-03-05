aca07bec062bc74f847cb31e80f8071ba310a94e
Harri Bell-Thomas <harribt@live.co.uk>
2018-03-05 01:27:20 +0000

    Fixes

--- chrome/inject.js
+++ chrome/inject.js
@@ -1,24 +1,31 @@
 window.onload = function() {
    // Inject the script
    var adaptiveScript = document.createElement("script");
-   adaptiveScript.src = "https://js.adaptive.org.uk/fresh/?negateglobal=N&mod=onDOMChange,adaptiveBase,adaptiveTools,linkHighlighter,motorFeatures,speedBar,typeWarning,colourTools,passwordReveal,darkMode,magnifier,showMouse,init";
+   adaptiveScript.src = "https://js.adaptive.org.uk/adaptive.js????";
    document.body.appendChild(adaptiveScript);
-   
+
    if (window.location.hash) {
       try {
          var hash = window.atob(window.location.hash.substr(1, window.location.hash.length - 1));
          var data = JSON.parse(hash);
-         console.log(data);
+         // console.log(data);
 
          if ("user_id" in data) {
             chrome.storage.local.set({"ADAPTIVE_A": data["user_id"]});
-            userID = data["user_id"];
+            //userID = data["user_id"];
          }
 
          if ("style_id" in data) {
             chrome.storage.local.set({"ADAPTIVE_B": data["style_id"]});
-            styleID = data["style_id"];
+            //styleID = data["style_id"];
          }
+
+
+         chrome.cookies.set({"name":"Sample1","url":"http://developer.chrome.com/extensions/cookies.html","value":"Dummy Data"},function (cookie){
+            console.log(JSON.stringify(cookie));
+            console.log(chrome.extension.lastError);
+            console.log(chrome.runtime.lastError);
+        });
       } catch(e) {}
    }
 };
