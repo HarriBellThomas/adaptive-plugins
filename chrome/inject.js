b995d29f4a67433f652e877eb57e6d0f4e95b1fb
Harri Bell-Thomas <harribt@live.co.uk>
2018-03-05 06:13:33 +0000

    Finishing off functionality

--- chrome/inject.js
+++ chrome/inject.js
@@ -1,31 +1,40 @@
+/* Runs every time the current tab location changes */
 window.onload = function() {
-   // Inject the script
-   var adaptiveScript = document.createElement("script");
-   adaptiveScript.src = "https://js.adaptive.org.uk/adaptive.js????";
-   document.body.appendChild(adaptiveScript);
+    // Retrieve items from local chrome storage
+    chrome.storage.local.get(['ADAPTIVE_A','ADAPTIVE_ENABLE'], function(result) {
+        if(result.ADAPTIVE_ENABLE != 0) {
 
-   if (window.location.hash) {
-      try {
-         var hash = window.atob(window.location.hash.substr(1, window.location.hash.length - 1));
-         var data = JSON.parse(hash);
-         // console.log(data);
+            /* If we have a user ID saved, inject it into the DOM */
+            if(result.ADAPTIVE_A !== undefined && result.ADAPTIVE_A !== "") {
+                var adaptiveAuth = document.createElement("script");
+                adaptiveAuth.textContent = "var _adaptiveAuth = '" + result.ADAPTIVE_A + "';";
+                document.body.appendChild(adaptiveAuth);
+            }
 
-         if ("user_id" in data) {
-            chrome.storage.local.set({"ADAPTIVE_A": data["user_id"]});
-            //userID = data["user_id"];
-         }
+            /* Inject the main adaptive script */
+            var adaptiveScript = document.createElement("script");
+            adaptiveScript.src = "https://js.adaptive.org.uk/adaptive.js?" + (+ new Date());
+            document.body.appendChild(adaptiveScript);
+        }
 
-         if ("style_id" in data) {
-            chrome.storage.local.set({"ADAPTIVE_B": data["style_id"]});
-            //styleID = data["style_id"];
-         }
+        /* If we see a page hash it might be out login return; check it */
+        if (window.location.hash) {
+            try {
+                var hash = window.atob(window.location.hash.substr(1, window.location.hash.length - 1));
+                var data = JSON.parse(hash);
 
+                /* Successfully found login data, save it */
+                if ("user_id" in data) {
+                    chrome.storage.local.set({"ADAPTIVE_A": data["user_id"]});
+                }
 
-         chrome.cookies.set({"name":"Sample1","url":"http://developer.chrome.com/extensions/cookies.html","value":"Dummy Data"},function (cookie){
-            console.log(JSON.stringify(cookie));
-            console.log(chrome.extension.lastError);
-            console.log(chrome.runtime.lastError);
-        });
-      } catch(e) {}
-   }
+                if ("style_id" in data) {
+                    chrome.storage.local.set({"ADAPTIVE_B": data["style_id"]});
+                }
+
+            } catch(e) {
+                //console.log("Error: " + e);
+            }
+        }
+    });
 };
